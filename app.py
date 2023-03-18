97b2e2e6b96ad3ae678de1556114b9feae508c62
Jiw <s6301012630150@email.kmutnb.ac.th>
2023-03-19 01:23:40 +0700

    inittial branch

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
 
-app = Flask(__name__)
-
+app = Flask(__name__) 
+#
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
